a0343d620f75feb751625889fde9a76d5fc76357
Kamil Janowski <kamil.janowski@reaktor.com>
2023-11-11 17:59:34 +0200

    changed to custom data set

--- pixieray/src/app/page.tsx
+++ pixieray/src/app/page.tsx
@@ -2,39 +2,61 @@
 
 import {useEffect, useState} from "react";
 import {Afe, SSEData} from "./event";
-import EyeChart, {EyeData} from "@/app/EyeChart";
+import EyeChart, {EyeData} from "./EyeChart";
+import ReactPlayer from "react-player";
 
 const MAX_ENTRIES_ON_CHART = 100;
+const BLINK_SUM = 90_000;
 
 const toEyeData = (afe: Afe): EyeData => ({
   timestamp: afe.i[1],
   position: afe.m[0]
 });
 
-export default function Home() {
+const isBlink = (afe: Afe): boolean => {
+  let sum = 0;
+  for (let i = 0; i < 6; i++) {
+    sum += afe.m[0][i];
+  }
 
+  return sum >= BLINK_SUM;
+}
+
+export default function Home() {
   const [data, setData] = useState<SSEData[]>([]);
+  const [run, setRun] = useState<boolean>(true);
+  const [blink, setBlink] = useState(false);
 
   useEffect(() => {
+    if (!run) {
+      return;
+    }
     const eventSource = new EventSource("http://localhost:8000/stream/driving", {
       withCredentials: true,
     });
+    let timeout: any;
     eventSource.addEventListener('data', (event) => {
       const record: SSEData = JSON.parse(event.data);
-      console.log(record);
       data.push(record);
 
       if (data.length > MAX_ENTRIES_ON_CHART) {
         data.shift();
       }
 
-      setData([...data])
+      setData([...data]);
+      if (isBlink(record.raw.afe[0]) || isBlink(record.raw.afe[1])) {
+        clearTimeout(timeout);
+        setBlink(true);
+      } else {
+        setBlink(false);
+      }
     })
     eventSource.onerror = (err) => {
       console.error(err);
+      setRun(false);
     }
     return () => eventSource.close();
-  }, []);
+  }, [run]);
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-6">
@@ -44,6 +66,25 @@ export default function Home() {
         </div>
         <div>
           <EyeChart eye="Right" data={data.map(d => toEyeData(d.raw.afe[1]))}/>
+        </div>
+        <div style={{width: '600px'}}>
+          <ReactPlayer url="http://localhost:3000/20231111_165747.mp4" controls={false} width="100%" playing={!!data.length && run} volume={0} />
+        </div>
+        <div>
+          <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded' onClick={() => {
+            if (run) {
+              setRun(false);
+            } else {
+              setData([]);
+              setRun(true);
+            }
+          }}>Pause/Restart</button>
+
+          <div className='text-4xl'>
+            {
+              blink ? 'Blink' : ''
+            }
+          </div>
         </div>
       </div>
     </main>
--- server/main.py
+++ server/main.py
@@ -5,8 +5,12 @@ from sse_starlette.sse import EventSourceResponse
 import os
 import ijson
 import simplejson as json
+import time
 
 app = FastAPI()
+
+def current_milli_time():
+    return round(time.time() * 1000)
 
 app.add_middleware(
     CORSMiddleware,
@@ -24,7 +28,7 @@ async def root():
 
 @app.get("/stream/driving")
 async def data_stream(request: Request):
-    file_path = os.path.join(os.path.dirname(__file__), '..', 'data', 'Driving', 'Participant_1', 'AFE_000_CONFIDENTIAL.json')
+    file_path = os.path.join(os.path.dirname(__file__), '..', 'data', '2023-11-11T16-57-48_Pixie27023_Junction-sushicat', 'AFE_000_uploaded.json')
 
     async def event_generator():
         with open(file_path, "rb") as f:
@@ -35,11 +39,9 @@ async def data_stream(request: Request):
                 current_timestamp = record['afe'][0]['i'][1]
                 time_to_wait_microseconds = current_timestamp - last_timestamp if last_timestamp != -1 else 0
                 last_timestamp = current_timestamp
-                print(time_to_wait_microseconds)
                 await asyncio.sleep(time_to_wait_microseconds / 1000000)
                 yield {
                     "event": "data",
-                    #"id": "message_id",
                     "data": json.dumps({
                         'raw': record
                     })
